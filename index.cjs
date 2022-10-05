8f9ec847551f47096ea2ec6de318090864340505
Giovanny <giovannyramirezs@hotmail.com>
2022-10-04 21:13:53 -0500

    Add comments

--- index.cjs
+++ index.cjs
@@ -9,10 +9,12 @@ const getKeys = () => {
   const pathPublicKey = path.join(__dirname, "./public-key");
   const pathPrivateKey = path.join(__dirname, "./private-key");
 
+  // Check if files exists
   let savedPublicKey = fs.existsSync(pathPublicKey);
   let savedPrivateKey = fs.existsSync(pathPrivateKey);
   
   if (!savedPublicKey || !savedPrivateKey) {
+    // If not exists, nodeRSA generates both and write and save both keys into folder
     const { privateKey, publicKey } = nodeRSA.createPrivateAndPublicKeys();
     savedPublicKey = publicKey;
     savedPrivateKey = privateKey;
@@ -21,6 +23,7 @@ const getKeys = () => {
     fs.writeFileSync("./public-key", publicKey);
   }
 
+  // Read the values of keys
   savedPublicKey = fs.readFileSync(pathPublicKey, { encoding: "utf8" });
   savedPrivateKey = fs.readFileSync(pathPrivateKey, { encoding: "utf8" });
 
@@ -32,9 +35,11 @@ const getKeys = () => {
 
 const { privateKey, publicKey } = getKeys();
 
+// Read message file
 const pathMessage = path.join(__dirname, "./message.txt");
 const inputText = fs.readFileSync(pathMessage, { encoding: "utf8" });
 
+// Encrypt and save message.txt into folder
 const encryptedText = nodeRSA.encryptStringWithRsaPublicKey({ 
   text: inputText, 
   publicKey,
@@ -43,6 +48,7 @@ const encryptedText = nodeRSA.encryptStringWithRsaPublicKey({
 console.log({ encryptedText });
 fs.writeFileSync("./cyphered-message.txt", encryptedText);
 
+// Decrypt cyphered-message.txt
 const decryptedText = nodeRSA.decryptStringWithRsaPrivateKey({ 
   text: encryptedText, 
   privateKey,
